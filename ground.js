246dec161f59ff2d768bea5a62c7a7dd06ee10cb
ArkMan13 <74531609+ArkMan13@users.noreply.github.com>
2021-02-02 15:40:31 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,28 @@
+class ground{
+    constructor(x,y,width,height){
+        var options={
+            isStatic:true  
+        }
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.body=Bodies.rectangle(this.x,this.y,this.width,this.height,options);
+        World.add(world,this.body);
+    }
+    display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(groundPos.x, groundPos.y);
+			rectMode(CENTER)
+			strokeWeight(4);
+			fill(255,255,0)
+			rect(0,0,this.w, this.h);
+			pop()
+			
+	}
+
+}
